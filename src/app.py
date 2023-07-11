fd14129994c90c6dc1a02d829e67e13a5bd4cd35
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-07-11 22:42:03 +0000

    semi-finished

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorites, Planet, Character
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,119 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+
+
+# GET ALL USERS
+@app.route('/user', methods=['GET'])
+def get_users():
+
+    users = User.query.all()
+    request_body = list(map(lambda x: x.serialize(), users))
+
+    return jsonify(request_body), 200
+
+
+
+# GET SPECIFIC USER
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_single_user(user_id):
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException("User doesn't exist", status_code=404)
+
+    user_data = {
+        'id': user1.id,
+        'username': user1.username,
     }
 
-    return jsonify(response_body), 200
+    return jsonify(user_data), 200
+
+
+
+# GET USERS/FAVORITES
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def user_favorites(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("User doesn't exist", status_code=404)
+    
+    favorites = user.favorites_id
+    favorites_data = []
+
+    for favorite in favorites:
+        favorite_data = {
+            'id': favorite.id,
+            'name': favorite.name
+        }
+        favorites_data.append(favorite_data)
+
+    return jsonify(favorites_data), 200
+
+# GET ALL CHARACTERS
+@app.route('/characters', methods=['GET'])
+def get_characters():
+
+    characters = Character.query.all()
+    request_body = list(map(lambda x: x.serialize(), characters))
+
+    return jsonify(request_body), 200
+
+# GETT SPECIFIC CHARACTER
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_single_character(character_id):
+
+    character = Character.query.get(character_id)
+    if character is None:
+        raise APIException("character doesn't exist", status_code=404)
+
+    character_data = {
+        'id': character.id,
+        'name': character.name,
+    }
+
+    return jsonify(character_data), 200
+
+# GET ALL PLANETS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    planets = Planet.query.all()
+    request_body = list(map(lambda x: x.serialize(), planets))
+
+    return jsonify(request_body), 200
+
+# GETT SPECIFIC CHARACTER
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_single_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException("planet doesn't exist", status_code=404)
+
+    planet_data = {
+        'id': planet.id,
+        'name': planet.name,
+    }
+
+    return jsonify(planet_data), 200    
+
+# /favorite/planet/<int:planet_id>
+# POST A FAVORITE PLANET ON AN USER
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+
+    # Get the current user (you can implement the logic to get the current user based on your authentication system)
+    current_user = User.query.get(current_user.id)  # Replace with your logic to get the current user
+
+    # Create a new favorite planet
+    new_favorite_planet = Planet(name='Planet Name', planet_id=planet_id)
+    current_user.favorites_id.favorite_planets = new_favorite_planet
+
+    # Save the changes to the database
+    db.session.add(new_favorite_planet)
+    db.session.commit()
+
+    return 'Favorite planet added successfully'
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,79 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites_id = relationship("Favorites", lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.username}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_id = db.Column(db.Integer, ForeignKey("user.id"), unique=True, nullable=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    favorite_planets= relationship('Planet', backref="user", uselist=False)
+    favorite_character= relationship('Character', backref="user", uselist=False)
+
+    def __repr__(self):
+        return f'<Favorites {self.name}>'
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+            # do not serialize the password, its a security breach
+        }
+    
+
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    character_id = db.Column(Integer, ForeignKey("favorites.id"))
+
+    def __repr__(self):
+        return f'<Character {self.name}>'
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+            # do not serialize the password, its a security breach
+        }    
+    
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    planet_id = db.Column(db.Integer, ForeignKey("favorites.id"))
+  
+
+    def __repr__(self):
+        return f'<Planet {self.name}>'
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
             # do not serialize the password, its a security breach
         }
